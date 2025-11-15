b3bdad286878b9416f70c8d4d417a84d825f535d
Zaibi448 <ukzoraib@gmail.com>
2025-11-15 10:10:13 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "ARSLAN-MD~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoibU1iWXFDRXFKU3locnAyOFNtTlY3d1BJdGxXVWtXYnBCV281aGk5NFJHaz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoibWFEbHhwQm1DamR6cHZVa043SG5KVjJZWWx2clUxNS93UndCZG9IOWZ5OD0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJhS2VablBDTmJhWDBBWHRTVU03aFRUVlJ0STYwb3Y1WGFHU2o0b3VvaVdZPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJZRndLcVd2TTdtUlZreFJKZ2VYaGp2TnRDSVVDVWN6Umk2T3ZJbWZ1M1FZPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IldGSGJoZEtnUS9EQWlZQitNN1lTdGtMMGE2SitKa1N3ajB0T1o2Z3ZBbG89In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImQ5alUxNWhiYmxVQTFHMmlCdGQ4aVRDVWxyMDd3dWFuWVBQakFLc2hjMHc9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoidUNLdk9sK05SMitkWnROS3lTVUJhY3g2djEzODBqSUFFL25sYmN3eXAyQT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSUhtWXhscngwT1ZDNm5xR0ZtclBXeG9jbGp0M0lmcEMrM2VFQ211eGlBWT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Im1wc3BmaXpEdGNnL3NoZmZDbE5uNXpLS1FreTVYUW43Zisyalhjc3NERWdScUZ0NjExNUpvVmFLRktsdTl4NWpVT29LYTdseE56bC9zbUpzdDM3dURBPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MjAsImFkdlNlY3JldEtleSI6ImRrS0FMVjlaVzdSZ1NsalhDMUk4R3o1N1R3aDdGTDQwKzFuSGtraTdubUk9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6ImVCdXVLcUNYUlVpZ182TnItbHJrU2ciLCJwaG9uZUlkIjoiNjk5NjZlYjctMzdiZi00Y2ZiLWI3MTgtYjJjNjUxMDc4NWU1IiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IndiZWxKQWVUS2RTTDRaVkhlUTFManBlQ2QwST0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJDKzUvaFJFMVJ4SVV2VzJESXMvK1k1SjM1bWs9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiOFBESFlIR1giLCJtZSI6eyJpZCI6IjkyMzQ3OTk4ODI4Nzo3NEBzLndoYXRzYXBwLm5ldCIsIm5hbWUiOiLGmMmR1Y/VsMqNw63JvsOtIiwibGlkIjoiMjczMzA2NjAzMjQ1ODAxOjc0QGxpZCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDT0hqMCtVRUVMbVQ0TWdHR0FFZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiQU9WbjhXZ29DdXVXeE1xN20rcnB3RXM5aWRkUDlIV0Evb3FBdUgybWtGcz0iLCJhY2NvdW50U2lnbmF0dXJlIjoiQWh6WHNKYnRuZm9RZU44R0YrRlVmejI4ZEVBZkJ6UmVqQWlYWk9JQXYvS1Nyclp3Z0VFWmRsS2NDMWNMaC84cWIyUmlqSDYrTlByTmZTMXVEUnlZREE9PSIsImRldmljZVNpZ25hdHVyZSI6InBMSThQSzRYa2FvZkhkRThnclVtZ3dtc2hNYmlqRmJXOFczUGRqMGtsbWhmOTdaVlI3VVRxRWFUWGxNTlFEYStIV3hrVGJQMXRZVlFuY01pOXJtWUR3PT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiOTIzNDc5OTg4Mjg3Ojc0QHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQlFEbFovRm9LQXJybHNUS3U1dnE2Y0JMUFluWFQvUjFnUDZLZ0xoOXBwQmIifX1dLCJwbGF0Zm9ybSI6InNtYmEiLCJyb3V0aW5nSW5mbyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkNBSUlDQWdTIn0sImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTc2MzE4MzA0NywibGFzdFByb3BIYXNoIjoiM1I5WjM5In0=",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -13,7 +13,7 @@ AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY KASHMIRI-AI 🇵🇰*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY ADEEL-MD 🇵🇰*",
 // set the auto reply massage on status reply  
 ANTI_DELETE: process.env.ANTI_DELETE || "false",
 // set true false for anti delete     
@@ -31,7 +31,7 @@ MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/hg5hj6.j
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "KASHMIRI-AI",
+BOT_NAME: process.env.BOT_NAME || "ADEEL-MD",
 // add bot namw here for menu
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // true to get auto status react
@@ -43,15 +43,15 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923247947238",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923035512967",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "ADEEL-MD",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*©POWERED BY KASHMIRI-AI*",
+DESCRIPTION: process.env.DESCRIPTION || "*©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴀᴅᴇᴇʟ-ᴍᴅ*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/hg5hj6.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> I'm alive*KASHMIRI-AI*🇵🇰",
+LIVE_MSG: process.env.LIVE_MSG || "> I'm alive*ADEEL-MD*🇵🇰",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
@@ -75,7 +75,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "923247947238",
+DEV: process.env.DEV || "923035512967",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
